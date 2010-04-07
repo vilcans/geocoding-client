6b268f3859e680e0f4aee563f3e97a1539710df2
Martin Vilcans <martin@librador.com>
2010-04-07 12:27:04 +0200

    Write to file instead of stdout

--- geocode.py
+++ geocode.py
@@ -12,8 +12,10 @@ query = u' '.join(a.decode() for a in sys.argv[1:])
 s = urllib.urlopen('http://maps.google.com/maps/api/geocode/json?address=' + urllib.quote(query.encode('utf-8')) + '&sensor=false')
 
 raw_data = s.read()
-sys.stderr.write(raw_data)
+sys.stdout.write(raw_data)
 j = json.loads(raw_data)
 stream = template.generate(json=j, query=query)
-print stream.render('xml').replace('kml:', '')
 
+with open((query + '.kml').encode(), 'w') as f:
+    # Google Earth doesn't support namespaces, so remove kml: prefix
+    f.write(stream.render('xml').replace('kml:', ''))
