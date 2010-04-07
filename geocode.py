1b23ce4e1522cf4db2f6bff586e3a06adb964071
Martin Vilcans <martin@librador.com>
2010-04-07 20:24:59 +0200

    Error handling and helpful messages

--- geocode.py
+++ geocode.py
@@ -2,8 +2,16 @@
 import sys
 import json
 import urllib
+try:
+    from genshi.template import TemplateLoader
+except ImportError:
+    sys.stderr.write("Genshi is required. Run 'easy_install genshi'.")
+    sys.exit(2)
 
-from genshi.template import TemplateLoader
+if len(sys.argv) < 2:
+    sys.stderr.write('Usage: \n    geocode.py <string to geocode>\n')
+    sys.exit(1)
+
 template = TemplateLoader(['.']).load('template.kml')
 
 query = u' '.join(a.decode() for a in sys.argv[1:])
@@ -15,6 +23,8 @@ sys.stdout.write(raw_data)
 j = json.loads(raw_data)
 stream = template.generate(json=j, query=query)
 
-with open((query + '.kml').encode(), 'w') as f:
+filename = query + '.kml'
+sys.stdout.write('Writing %s\n' % filename)
+with open(filename.encode(), 'w') as f:
     # Google Earth doesn't support namespaces, so remove kml: prefix
     f.write(stream.render('xml').replace('kml:', ''))
